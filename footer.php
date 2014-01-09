59eca17d0c6c97eef1ee3347a8600ac92af9532a
rue89lyon <githubrue89lyon>
2014-01-09 09:56:55 +0100

    Appartement 16 - 08/01/14
    Mise en place du background et de l’entête png transparent

--- footer.php
+++ footer.php
@@ -1,21 +1,24 @@
-	</div><!-- Row End -->
-</div><!-- Container End -->
+</div>
+<!-- Row End -->
+</div>
+<!-- Container End -->
 
 <footer class="full-width" role="contentinfo">
-	<div class="row">
-		<div class="large-12">
-			
-		</div>
-	</div>
+    <div class="row">
+        <div class="large-12">
+
+        </div>
+    </div>
 </footer>
 
 <?php wp_footer(); ?>
 
 <script>
-	(function($) {
-		$(document).foundation();
-	})(jQuery);
+    (function($) {
+    		$(document).foundation();
+    	})(jQuery);
 </script>
-	
+
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -1,29 +1,44 @@
 <?php get_header(); ?>
 
 <!-- Row for main content area -->
-	<div class="small-12 large-8 columns" id="content" role="main">
-	
-	<?php if ( have_posts() ) : ?>
-	
-		<?php /* Start the Loop */ ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'content', get_post_format() ); ?>
-		<?php endwhile; ?>
-		
-		<?php else : ?>
-			<?php get_template_part( 'content', 'none' ); ?>
-		
-	<?php endif; // end have_posts() check ?>
-	
-	<?php /* Display navigation to next/previous pages when applicable */ ?>
-	<?php if ( function_exists('reverie_pagination') ) { reverie_pagination(); } else if ( is_paged() ) { ?>
-		<nav id="post-nav">
-			<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'reverie' ) ); ?></div>
-			<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'reverie' ) ); ?></div>
-		</nav>
-	<?php } ?>
-
+	<div class="row large-12 columns center">
+        <img src="<?php bloginfo('template_url'); ?>/images/croix-depart-appartement16-2014.png" alt="appartement16 lyon">
+        <img class="center" src="<?php bloginfo('template_url'); ?>/images/cerf-appartementt16-2014.png" alt="appartement16 lyon">
+        <img src="<?php bloginfo('template_url'); ?>/images/logo-appartement16-2014.png" alt="appartement16 lyon">
+        <img src="<?php bloginfo('template_url'); ?>/images/biais-noir-appartement16-2014.png" alt="appartement16 lyon">
 	</div>
-	<?php get_sidebar(); ?>
-		
-<?php get_footer(); ?>
+    <div class="row large-12 columns backgrd-black">
+        <div class=" large-4 columns padding-image">
+            <img src="<?php bloginfo('template_url'); ?>/images/titre-il-etait-une-fois-appt-16-2014.png" alt="appartement16 lyon">
+        </div>
+        <div class=" large-8 columns backgrd-black padding-image"> 
+            <p>
+            … un salon de coiffure du nom d’Appartement 16.<br />
+    
+    L’envie de travailler différemment, de considérer l’apparence comme un facteur essentiel de bien-être intérieur qui nécessite du temps, qui, touchant à l’intime de la personne, se doit d’être pris en charge avec attention, dans le confort et la douceur : comme à la maison. Obsolètes, le salon «vitrine», la conformité des univers aseptisés, le bruit assourdissant des sèches-cheveux que couvre à peine la radio pourtant hurlante…<br /><br />
+    
+    L’appartement 16 : un nom comme une adresse. Un lieu pensé en commun où chaque pièce est le reflet de «l’habitant».Vous savez qui vous coiffe en regardant ses murs. Ce que vous désirez : chacun ici prendra le temps de l’entendre et vous accompagnera, du premier au dernier pas, parce que c’est aussi cela un rendez-vous : un tête à tête.<br /><br />
+    Les produits sont choisis avec soin, dans le souci constant du respect du cheveux comme une matière… vivante.<br />
+    La coiffure comme l’art est création. Aussi, il a semblé important d’ouvrir le salon à de jeunes artistes lyonnais et d’offrir ses murs comme support d’expression.
+    <br /><br />
+    C’est l’histoire d’un salon atypique, d’une intimité préservée, d’un moment dont la magie reste accessible à toutes et tous. Aujourd’hui, avec la confiance de ses clients, l’appartement 16 est un salon qui a dépassé le simple concept.
+            </p>
+        </div>
+    </div>
+</div>
+<!-- Row End -->
+
+</div>
+<!-- Container End -->
+
+<?php wp_footer(); ?>
+
+<script>
+    (function($) {
+    		$(document).foundation();
+    	})(jQuery);
+</script>
+
+</body>
+
+</html>
